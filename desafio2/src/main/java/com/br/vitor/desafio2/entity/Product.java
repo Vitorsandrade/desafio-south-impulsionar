73d93b928a57156dac8e0ebc3d344e5e512d236d
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-11-03 16:25:18 -0300

    atributos

--- desafio2/src/main/java/com/br/vitor/desafio2/entity/Product.java
+++ desafio2/src/main/java/com/br/vitor/desafio2/entity/Product.java
@@ -3,18 +3,29 @@ package com.br.vitor.desafio2.entity;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.io.Serializable;
+import java.math.BigDecimal;
 
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-@With
 @Entity
-public class Product {
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(exclude = {"id"})
+public class Product implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
 
     @Id
-    private Integer id;
-
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private BigDecimal price;
+    private String category;
+    private String name;
+    private Integer amount;
 
 }
