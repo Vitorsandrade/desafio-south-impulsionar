8ba9d70743a0ddc1ecc7043f993df5120131f237
vitorsandrade <vitorsantos.dandrade@gmail.com>
2022-12-28 11:50:57 -0300

    feature/desafio2

--- desafio2/src/main/java/com/br/vitor/desafio2/ProdutoApiApplication.java
+++ desafio2/src/main/java/com/br/vitor/desafio2/ProdutoApiApplication.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Desafio2Application {
+public class ProdutoApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Desafio2Application.class, args);
+        SpringApplication.run(ProdutoApiApplication.class, args);
 
 
     }
